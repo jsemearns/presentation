84e77ffc9b3b3a2cee6f554ebd6f278e41326140
Justin Symon Mearns <justin.mearns@gmail.com>
2019-10-21 09:45:54 -0600

    added tetris game

--- jscanvas/urls.py
+++ jscanvas/urls.py
@@ -2,13 +2,16 @@ from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
 from speechtocanvas import views
+
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
+    "",
     # Examples:
     # url(r'^$', 'jscanvas.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', views.HomePageView.as_view(), name='home')
+    url(r"^admin/", include(admin.site.urls)),
+    url(r"^$", views.HomePageView.as_view(), name="home"),
+    url(r"^snake/", views.SnakeTemplateView.as_view(), name="snake"),
+    url(r"^tetris/", views.TetrisTemplateView.as_view(), name="tetris"),
 )
--- speechtocanvas/views.py
+++ speechtocanvas/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render
 from django.views.generic import TemplateView
+
 
 # Create your views here.
 class HomePageView(TemplateView):
-    template_name = 'canvas.html'
+    template_name = "index.html"
+
+
+class SnakeTemplateView(TemplateView):
+    template_name = "canvas.html"
+
+
+class TetrisTemplateView(TemplateView):
+    template_name = "tetris.html"
