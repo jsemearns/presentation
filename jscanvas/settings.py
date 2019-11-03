9fa0558f06a1af2ff5ae20cff238b73bed30cbc7
Justin Symon Mearns <justin.mearns@gmail.com>
2019-11-03 01:30:04 -0700

    update

--- jscanvas/settings.py
+++ jscanvas/settings.py
@@ -83,4 +83,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = [os.path.join(BASE_DIR, "static"), "/var/www/static/"]
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
